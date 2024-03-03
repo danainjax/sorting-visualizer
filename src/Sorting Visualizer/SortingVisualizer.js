04c3ea64590378f898f15d6885f301ba119b8f8b
danathedev <danainjax@gmail.com>
2024-03-02 21:08:10 -0500

    randomize array length and numbers to fill the array

--- src/Sorting Visualizer/SortingVisualizer.js
+++ src/Sorting Visualizer/SortingVisualizer.js
@@ -1,5 +1,31 @@
+import { useState } from 'react';
+
 const SortingVisualizer = () => {
-  return <p>Sorting Visualizer</p>;
+  const generateRandomNumbers = () => {
+    const length = Math.floor(Math.random() * (1000 - 10 + 1)) + 10;
+    const array = Array.from(
+      { length },
+      () => Math.floor(Math.random() * (1000 - 5 + 1)) + 5
+    );
+
+    return array;
+  };
+
+  const [randomNumbers, setRandomNumbers] = useState(generateRandomNumbers());
+
+  return (
+    <div>
+      <h2>Sorting Visualizer</h2>
+      <button onClick={() => setRandomNumbers(generateRandomNumbers())}>
+        Generate New Numbers
+      </button>
+      <p>Array Length: {randomNumbers.length}</p>
+
+      {randomNumbers.map((number, index) => (
+        <div key={index}>{number}</div>
+      ))}
+    </div>
+  );
 };
 
 export default SortingVisualizer;
